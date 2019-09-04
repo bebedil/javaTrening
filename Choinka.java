0cf7e1bf40e90a5881d0a9ec228248903615ac0a
unknown <boguslaw.brzezny@gmail.com>
2019-09-04 22:42:04 +0200

    budowanie choinki

---
+++ Choinka.java
@@ -0,0 +1,12 @@
+package bb.bbjava.poczatek.javaTrening;
+
+public class Choinka {
+    public static void main(String[] args) {
+
+        int size = 5;
+        System.out.println(size);
+
+    }
+
+
+}
