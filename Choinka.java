d7c9953554bc756df8c02c9a3bdb15cc45637006
bogus <boguslaw.brzezny@gmail.com>
2019-09-06 22:23:43 +0200

    sculpture

--- Choinka.java
+++ Choinka.java
@@ -3,10 +3,23 @@ package bb.bbjava.poczatek.javaTrening;
 public class Choinka {
     public static void main(String[] args) {
 
-        int size = 5;
-        System.out.println(size);
+        int size = 1;
+        int maxHigh = 1;
 
+        for (int seg = 1; seg <= size; seg++) {
+            for (int row = 0; row < seg - Math.max(seg - maxHigh, 0); row ++) {
+                for (int i = 0; i <= size - Math.max(seg - maxHigh, 0) - row; i++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2 * (row + 1 + Math.max(seg - maxHigh, 0)) ; j++) {
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+        }
+        for (int k = 0; k <= size; k++) {
+            System.out.print(" ");
+        }
+        System.out.print("##");
     }
-
-
 }
