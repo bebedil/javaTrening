99da8e3b72fd7ca57128054db2fa2baf71cd085b
bogus <boguslaw.brzezny@gmail.com>
2019-09-18 18:44:48 +0200

    zadanie 6. obliczanie silni

---
+++ rev/Rev.java
@@ -0,0 +1,20 @@
+package bb.bbjava.poczatek.javaTrening.rev;
+
+// nie wiem dlaczego nie działa u mnie charAt
+// zaminiłem to na tablicę i użyłem pomocniczo zmiennych temp i revtemp
+// jeszcze sprobuje z charAt...
+
+public class Rev {
+    public static void main(String[] args) {
+        String output = reverse("Testowanie");
+        System.out.println(output);
+    }
+    private static String reverse(String input) {
+        char[] temp = input.toCharArray();
+        String revtemp = "";
+        for (int i = (temp.length - 1); i >= 0; i--) {
+            revtemp = revtemp + temp[i];
+        }
+        return revtemp;
+    }
+}
