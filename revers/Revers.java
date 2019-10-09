fcda82872c5ad5c1791540a50e8b598a03020b45
bogus <boguslaw.brzezny@gmail.com>
2019-10-09 11:07:22 +0200

    moja wersja niestety musiałem spojrzeć na rozwiązanie :-(

--- revers/Revers.java
+++ revers/Revers.java
@@ -1,44 +1,39 @@
 package bb.bbjava.poczatek.javaTrening.revers;
 
-// wersja z charAt
-
 public class Revers {
     public static void main(String[] args) {
-        String revtemp = "ogórek";
-        String input1 = "pomidor";
-        boolean output = false;
-        reverse(cut("kobyla ma maly bok"));
-        //isPalindrome(reverse(cut("kobyla ma maly bok")));
-        System.out.println(revtemp);
-        System.out.println(input1);
-        System.out.println(output);
+
+        String toCheck = "kobyla ma maly bok";
+        System.out.println(isPalindrome(toCheck));
     }
-    private static String reverse(String input1) {
+
+    static boolean isPalindrome(String toCheck) {
+        String cut = cut(toCheck);
+        return cut.equals(reverse(cut));
+    }
+
+    static String reverse(String input1) {
         String revtemp = "";
         for (int i = (input1.length() - 1); i >= 0; i--) {
             revtemp = revtemp + input1.charAt(i);
         }
-        System.out.println(revtemp);
         return revtemp;
     }
 
-    private static String cut(String input) {
+    //static String cut(String input) {
+    //String input1 = "";
+    //for (int j = 0; j < input.length(); j++) {
+    //    if (input.charAt(j) != ' ')
+    //        input1 = input1 + input.charAt(j);
+    static String cut(String toCheck) {
         String input1 = "";
-        for (int j = 0; j < input.length(); j++) {
-            if (input.charAt(j) != ' ')
-                input1 = input1 + input.charAt(j);
+        for (int j = 0; j < toCheck.length(); j++) {
+            if (toCheck.charAt(j) != ' ')
+                input1 = input1 + toCheck.charAt(j);
+
         }
-        System.out.println(input1);
         return input1;
     }
 
-    private static boolean isPalindrome(String revtemp, String input1) {
-        boolean result = false;
-        if (input1 == revtemp) {
-            return result = true;
-        } else {
-            return result = false;
-        }
-    }
 }
 
